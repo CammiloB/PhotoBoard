88b3c42b87097cc7964cb12e829d2b5022d2de77
CammiloB <sergio.camilo.blanco@gmail.com>
2020-04-26 21:58:38 -0500

    Add Calendar

--- lib/main.dart
+++ lib/main.dart
@@ -1,243 +1,73 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:avatar_glow/avatar_glow.dart';
+import 'package:flutter/scheduler.dart' show timeDilation;
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:ejemplo_construccion/delayed_animation.dart';
-import 'package:ejemplo_construccion/login.dart';
-import 'package:ejemplo_construccion/register.dart';
+import 'package:ejemplo_construccion/login/flutter_login.dart';
+import 'package:ejemplo_construccion/login/src/users.dart';
 
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
+import 'package:ejemplo_construccion/home/screens/home_page.dart';
+import 'transition_route_observer.dart';
+import 'custom_route.dart';
 
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setEnabledSystemUIOverlays([]);
+void main() => runApp(new MyApp());
 
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
+class MyApp extends StatelessWidget {
+  Duration get loginTime => Duration(milliseconds: timeDilation.ceil() * 2250);
 
-  runApp(MaterialApp(
-    title: "PhotoBoard",
-    home: MyApp(
-      camera: firstCamera
-    )
-  ));
-}
+  /*Future<String> _loginUser(LoginData data) {
+    return Future.delayed(loginTime).then((_) {
+      if (!mockUsers.containsKey(data.name)) {
+        return 'Username not exists';
+      }
+      if (mockUsers[data.name] != data.password) {
+        return 'Password does not match';
+      }
+      /* Navigator.of(context).push(
+        MaterialPageRoute<void> (
+          builder: (BuildContext context) => HomePage()
+        
+        )
+      ); */
+      return null;
+    });
+  }*/
 
-class MyApp extends StatefulWidget {
-  final CameraDescription camera;
-
-  const MyApp({Key key, @required this.camera}): super(key: key);
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-
-
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
-  final int delayedAmount = 500;
-  double _scale;
-  AnimationController _controller;
-  @override
-  void initState() {
-    _controller = AnimationController(
-      vsync: this,
-      duration: Duration(
-        milliseconds: 200,
-      ),
-      lowerBound: 0.0,
-      upperBound: 0.1,
-    )..addListener(() {
-        setState(() {});
-      });
-    super.initState();
+  Future<String> _loginUser(LoginData data){
+    return Future.delayed(loginTime).then((_) async {
+      try{
+        AuthResult result = await FirebaseAuth.instance.signInWithEmailAndPassword(email: data.name, password: data.password);
+        return null;
+      }catch (e){
+        return "Error";
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    final color = Colors.white;
-    _scale = 1 - _controller.value;
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body: Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [Colors.black87, Colors.blueGrey[200]]),
-            ),
-            child: ListView(
-              children: <Widget>[
-                Center(
-                child:Column(
-                children: <Widget>[
-                 AvatarGlow(
-                  endRadius: 90,
-                  duration: Duration(seconds: 2),
-                  glowColor: Colors.white24,
-                  repeat: true,
-                  repeatPauseDuration: Duration(seconds: 2),
-                  startDelay: Duration(seconds: 1),
-                  child: Material(
-                      elevation: 8.0,
-                      shape: CircleBorder(),
-                      child: CircleAvatar(
-                        backgroundColor: Colors.blueGrey[700],
-                        child: new Image.asset('assets/foto1.png', width: 120.0, height: 145.0,),
-                        radius: 65.0,
-                      )),
-                ),
-                DelayedAnimation(
-                  child: Text(
-                    "Bienvenido a",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 35.0,
-                        color: color),
-                  ),
-                  delay: delayedAmount + 1000,
-                ),
-                DelayedAnimation(
-                  child: Text(
-                    "PhotoBoard",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 35.0,
-                        color: color),
-                  ),
-                  delay: delayedAmount + 2000,
-                ),
-                SizedBox(
-                  height: 30.0,
-                ),
-                SizedBox(
-                  height: 100.0,
-                ),
-                DelayedAnimation(
-                child: GestureDetector(
-                  onTapDown: _onTapDown,
-                  onTapUp: _onTapUp,
-                  child: Transform.scale(
-                    scale: _scale,
-                    child: _loginButtonUI,
-                  ),
-                ),
-                delay: delayedAmount + 4000,
-              ),
-              SizedBox(height: 50.0,),
-                DelayedAnimation(
-                child: GestureDetector(
-                  onTapDown: _onTapDown,
-                  onTapUp: _onTapUp,
-                  child: Transform.scale(
-                    scale: _scale,
-                    child: _registerButtonUI,
-                  ),
-                ),
-                delay: delayedAmount + 4000,
-              ),
-                ]
-                ),
-                )
-              ],
-            ),
-          )
-          ),
+    return new MaterialApp(
+      title: 'Login',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FlutterLogin(
+        onSignup: null,
+        onLogin: (loginData) {
+          print('Login info');
+          print('Name: ${loginData.name}');
+          print('Password: ${loginData.password}');
+          return _loginUser(loginData);
+        },
+        onRecoverPassword: null,
+        onSubmitAnimationCompleted: () {
+        Navigator.of(context).pushReplacement(FadePageRoute(
+          builder: (context) => HomePage(),
+        ));
+      },
+      ),
+      navigatorObservers: [TransitionRouteObserver()],
+      routes: {HomePage.routeName: (context) => HomePage()},
     );
-  }
-
-  Widget get _loginButtonUI => Container(
-        height: 60,
-        width: 270,
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.red[300],
-              blurRadius: 10.0, // has the effect of softening the shadow
-              spreadRadius: 1.0, // has the effect of extending the shadow
-              offset: Offset(
-                5.0, // horizontal, move right 10
-                5.0, // vertical, move down 10
-              ),
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute<void>(
-                builder: (BuildContext context) => LoginPage(
-                  camera: widget.camera
-                )
-              )
-            );
-          },
-          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-          child: const Text(
-            'Ingresar',
-            style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w700,
-                ),
-          ),
-        ),
-      );
-
-    Widget get _registerButtonUI => Container(
-        height: 60,
-        width: 270,
-         decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.red[300],
-              blurRadius: 10.0, // has the effect of softening the shadow
-              spreadRadius: 1.0, // has the effect of extending the shadow
-              offset: Offset(
-                5.0, // horizontal, move right 10
-                5.0, // vertical, move down 10
-              ),
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(30),
-        ),
-        child: RaisedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-              MaterialPageRoute<void>(
-                builder: (BuildContext context) => RegisterPage(
-                  camera: widget.camera,
-                )
-              )
-            );
-          },
-          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
-          child: const Text(
-            'Registrarse',
-            style: TextStyle(
-                  color: Colors.blueGrey,
-                  fontSize: 14,
-                  fontWeight: FontWeight.w700,
-                ),
-          ),
-        ),
-      );
-
-  void _onTapDown(TapDownDetails details) {
-    _controller.forward();
-  }
-
-  void _onTapUp(TapUpDetails details) {
-    _controller.reverse();
   }
 }
